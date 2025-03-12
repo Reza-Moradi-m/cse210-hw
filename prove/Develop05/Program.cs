3a272390524ab905c7d9649bad4d4eadd807a8f4
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2025-03-12 15:47:35 -0600

    d

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Menu menu = new Menu();
+        while (true)
+        {
+            menu.DisplayMenu();
+            Console.Write("\nEnter your choice: ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    menu.CreateGoal();
+                    break;
+                case "2":
+                    menu.RecordGoalEvent();
+                    break;
+                case "3":
+                    menu.DisplayGoals();
+                    break;
+                case "4":
+                    menu.Save();
+                    break;
+                case "5":
+                    menu.Load();
+                    break;
+                case "6":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
     }
 }
